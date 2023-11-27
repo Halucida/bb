162e97124e87c83a721ff0389e18119aebc8feaf
Kepin <141326351+Internite@users.noreply.github.com>
2023-11-27 13:12:47 +0700

    Hii !

---
+++ Moline/Hela.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import json, requests
+
+def Modeler(test_data, url, output="max"):
+    data = json.dumps({"signature_name":"serving_default", "instances":test_data})
+    json_resp = requests.post(url, data=data, headers={"content-type":"application/json"})
+    json_load = json.loads(json_resp.text)
+    result = json_load["predictions"]
+    if output == "max":
+        return tf.math.argmax(result, axis=1).numpy()
+    elif output == "original":
+        return result
+
+def Helost(label, logits, loss_fn):
+    loss = loss_fn(label, logits)
+    return loss.numpy()
---
+++ Utili/Helpin.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.utils import pad_sequences
+
+from stop_words import get_stop_words
+
+def Helist(sentence, lang="en"):
+    return result
+
+def Helpad(token, sentence, **helargs):
+    return result
