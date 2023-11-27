57bd69c7fe9b63e29d727b622c2586caf17c009f
Kepin <141326351+Internite@users.noreply.github.com>
2023-11-27 14:22:35 +0700

    Hii !

--- Utili/Helpin.py
+++ Utili/Helpin.py
@@ -2,8 +2,13 @@ from tensorflow.keras.utils import pad_sequences
 
 from stop_words import get_stop_words
 
-def Helist(sentence, lang="en"):
+def Helist(sentence):
+    untest = sentence.lower().split()
+    united = [i for i in untest if i not in unlist]
+    result = " ".join(united)
     return result
 
 def Helpad(token, sentence, **helargs):
+    temp = token.texts_to_sequences(sentence)
+    result = pad_sequences(temp, **helargs)
     return result
